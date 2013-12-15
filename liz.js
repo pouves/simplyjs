2c3a340973ca478eb2ab1821706c4012a136c28c
pouves <elizabethpollom@gmail.com>
2013-12-15 14:29:24 -0800

    Create liz.js
    creating liz.js

---
+++ liz.js
@@ -0,0 +1,26 @@
+console.log('Simply.js demo!');
+
+simply.on('singleClick', function(e) {
+  console.log(util2.format('single clicked $button!', e));
+  simply.setText({
+    subtitle: 'Pressed ' + e.button + '!',
+  });
+});
+
+simply.on('longClick', function(e) {
+  console.log(util2.format('long clicked $button!', e));
+});
+
+simply.on('accelTap', function(e) {
+  console.log(util2.format('tapped accel axis $axis $direction!', e));
+  simply.setText({
+    subtitle: 'Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!',
+  });
+});
+
+simply.setText({
+  title: 'Simply Demo!',
+  body: 'This is a demo. Press buttons or tap the watch!',
+}, true);
+
+simply.begin();
