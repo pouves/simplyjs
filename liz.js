c865d654531bc4bef80b023147cd2007cd1d0311
pouves <elizabethpollom@gmail.com>
2014-01-08 23:22:30 -0800

    Update liz.js

--- liz.js
+++ liz.js
@@ -2,13 +2,26 @@
 console.log('Simply.js demo!');
 
 simply.on('singleClick', function(e) {
-if (e.button === 'select') {
-		simply.setText({body: 'awesome'});
-} else if (e.button === 'up') {
-		simly.setText({subtitle: '(^-^)' });
-	} else {
-		simply.setText({title: 'Goodbye'});
-};
+  console.log(util2.format('single clicked $button!', e));
+  simply.setText({
+    subtitle: 'Pressed ' + e.button + '!',
+  });
+});
+
+simply.on('longClick', function(e) {
+  console.log(util2.format('long clicked $button!', e));
+});
+
+simply.on('accelTap', function(e) {
+  console.log(util2.format('tapped accel axis $axis $direction!', e));
+  simply.setText({
+    subtitle: 'Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!',
+  });
+});
+
+simply.setText({
+  title: 'Simply Demo!',
+  body: 'This is a demo. Press buttons or tap the watch!',
+}, true);
 
 simply.begin();
-
