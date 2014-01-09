d9e037afd2eb0c390a3d5f9db9bba479e97ffc07
pouves <elizabethpollom@gmail.com>
2014-01-08 23:18:08 -0800

    Update liz.js

--- liz.js
+++ liz.js
@@ -1,10 +1,10 @@
-simply.on(‘singleClick’, function(e)){
-if (e.button === ‘select’) {
-		simply.setText({body: ‘awesome’});
-} else if (e.button === ‘up’) {
-		simly.setText({subtitle: ‘(^-^)’ });
+simply.on('singleClick', function(e)){
+if (e.button === 'select') {
+		simply.setText({body: 'awesome'});
+} else if (e.button === 'up') {
+		simly.setText({subtitle: '(^-^)' });
 	} else {
-		simply.setText({title: ‘Goodbye’});
+		simply.setText({title: 'Goodbye'});
 };
 
 simply.begin();
